43c355e717ffa62bca5aa3f193ce2af1c4542ff6
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-10-18 02:28:03 -0600

    Clase funcional

--- PruebaTecnicaSngular/src/components/FunctionComponents/FunctionCoponents.js
+++ PruebaTecnicaSngular/src/components/FunctionComponents/FunctionCoponents.js
@@ -1,35 +1,51 @@
-//Fibonacci function
-// function nFibonacci(n) {
-//     if(n < 2) return n;
-//     return nFibonacci(n-1) + nFibonacci(n-2);
-// }
+class NSerie {
+    static nTriangular(n) {
+        return n * (n + 1) / 2
+    }
 
+    static nPrimos(n) {
+        function esPrimo(n) {
+            let numFactores = 0;
+            for (let i = 1; i <= n; i++) {
+                if (n % i === 0) numFactores = numFactores + 1;
+            }
 
-// Número triangular serie 
-// function nTriangular(n) {
-//     return n*(n + 1) / 2
-// }
+            if (numFactores === 2) return 'si';
+            else return 'no';
 
-// Primos Function
-// function nPrimos(n) {
-//     function esPrimo(n) {
-//         let numFactores = 0;
-//         for (let i = 1; i <= n; i++) {
-//             if (n % i === 0) numFactores = numFactores + 1;
-//         }
+        }
 
-//         if (numFactores === 2) return 'si';
-//         else return 'no';
+        let numPrimos = 0;
+        let j = 0;
+        while (numPrimos < n) {
+            if (esPrimo(j) === 'si') numPrimos = numPrimos + 1;
+            j = j + 1;
+        }
+        return j - 1;
+    }
 
-//     }
+    static nFibonacci(n) {
+        if (n <= 1) return n; 
+ 
+        let a = 0; 
+        let b = 1; 
+        for (let i = 2; i <= n; i++) { 
+            const temp = a + b; 
+            a = b; 
+            b = temp; 
+        } 
+ 
+        return b; 
+    }
 
-//     let numPrimos = 0;
-//     let j = 0;
-//     while (numPrimos < n) {
-//         if (esPrimo(j) === 'si') numPrimos = numPrimos + 1;
-//         j = j + 1;
-//     }
-//     return j - 1;
-// }
+    static serieResultados (n) {
+        const sTriangular = NSerie.nTriangular(n+1);
+        const sPrimos = NSerie.nPrimos(n);
+        const sFibonacci = NSerie.nFibonacci(n+1);
+        
+        return (2* sTriangular) - (2* sPrimos) + sFibonacci;
+    }
 
-// console.log(nPrimos(11));
+}
+
+console.log(NSerie.serieResultados(10))
